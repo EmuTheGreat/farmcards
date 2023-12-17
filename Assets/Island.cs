b6591bf3f26221cea0733aae4ec144bc4375065a
DmitryLysikov <129677338+DmitryLysikov@users.noreply.github.com>
2023-12-17 20:45:16 +0500

    fdsfsd

--- Assets/Island.cs
+++ Assets/Island.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Island 
 {
     public List<Vector2Int> placementDatas;
-    public Vector2Int islandPos;
+    public Vector3 islandPos;
 
-    public Island(List<Vector2Int> vector2Ints, Vector2Int vector2Int)
+    public Island(List<Vector2Int> vector2Ints, Vector3 vector2Int)
     {
         placementDatas = vector2Ints;
         islandPos = vector2Int;
--- Assets/SaveObk.cs
+++ Assets/SaveObk.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SaveObk : MonoBehaviour
@@ -9,10 +13,33 @@ public class SaveObk : MonoBehaviour
 
     public void Save()
     {
-        for(var i = 0; i < islandlist.transform.childCount; i++)
+        List<IslandBuilding> islandBuildings = new List<IslandBuilding>();
+        for (var i = 0; i < islandlist.transform.childCount; i++)
         {
-           var temp =   islandlist.transform.GetChild(i).GetComponent<IslandBuilding>();
-
+            var temp = islandlist.transform.GetChild(i).GetComponent<IslandBuilding>();
+           islandBuildings.Add(temp);
         }
+        SerializeAndSaveToJson(islandBuildings, "save.data");
+    }
+
+    static void SerializeAndSaveToJson<T>(T obj, string filePath)
+    {
+        string jsonString = JsonSerializer.Serialize(obj);
+
+        // Сохраняем JSON-строку в файл
+        File.WriteAllText(filePath, jsonString);
+    }
+
+    public void Load()
+    {
+        string filePath = "save.data";
+        DeserializeFromJson(filePath);
+    }
+
+    static T DeserializeFromJson<T>(string filePath)
+    {
+        string jsonString = File.ReadAllText(filePath);
+
+        return JsonSerializer.Deserialize<T>(jsonString);
     }
 }
